7f65b25084a4c941942b6ff5a707b2953a967475
Mark Drobnak <mtd8050@rit.edu>
2021-03-31 17:33:42 -0400

    Read more than just one line (until Ctrl-D or EOF) as input

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate lalrpop_util;
 use crate::lexer::{LalrpopLexerIter, Token};
 use logos::Logos;
 use std::io;
+use std::io::Read;
 
 mod lexer;
 
@@ -14,7 +15,7 @@ lalrpop_mod!(
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
+    io::stdin().read_to_string(&mut input).unwrap();
 
     let lexer = Token::lexer(&input);
     let lexer_iter = LalrpopLexerIter::new(lexer);
